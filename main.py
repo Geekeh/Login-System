3a0f32a49c953bc97d42960d7ee7077afe458e61
Geek <98252854+Geekeh@users.noreply.github.com>
2022-12-12 16:34:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 from flask import Flask, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-keys = ['123']
+mongo_url = 'mongo connection url'
+database = MongoClient(mongo_url)
+db = database['database_name']
+collection = db['collection_name']
+
 
 @app.route('/')
 def index():
-    return 'use /api'
+  return 'use /api'
+
 
 @app.route("/api", methods=["POST", "GET"])
 def api_page():
     if request.method == "POST":
         headers = request.headers
-        if "key" not in headers:
-            return ({"error":"invalid headers"})
+        if "key" not in headers or "hwid" not in headers:
+            return ({"error": "invalid headers"})
 
-        user_keys = request.headers.get('key')
-        if user_keys not in keys:
+        user_key = request.headers.get("key")
+        database = collection.find()
+        all_keys = []
+
+        for x in database:
+            all_keys.append(x['key'])
+
+        if user_key not in all_keys:
             return ({"error": "invalid key"})
-        return ({"valid key":user_keys})
+
+        key_info = collection.find_one({"key": user_key})
+        if headers.get('hwid') == key_info['hwid']:
+            key_info = collection.find_one({"key": user_key})
+            return ({
+                "valid_key": user_key,
+                "hwid": headers.get('hwid'),
+                "ip": key_info['ip']
+            })
+        else:
+            return ({"error": "invalid headers"})
     else:
-        return "404 not found"
+        return "invalid method used"
 
 
 app.run(host='0.0.0.0', port=81)
